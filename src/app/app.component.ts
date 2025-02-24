c969756777b7f8ebbded12d8f7b8dba381805b0d
Evertonildo Maia <evertonildo@gmail.com>
2025-02-24 11:07:51 -0300

    site novo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import * as exampleData from './assets/app.json';
+import { MatTabsModule } from '@angular/material/tabs';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MatTabsModule, MatIconModule, MatButtonModule, MatToolbarModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  links: any[] = exampleData.links;
+  ngOnInit(): void {
+    console.log('links', this.links);
+  }
   title = 'my-favorities';
+  styling(item: any){
+    return `color: ${item.color}; background-color: ${item.backcolor}; ${item.style}`;
+  }
 }
